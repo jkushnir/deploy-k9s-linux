bb201cd6cb6103241169f9a9aec94f21d5bc5410
Yevgeny Kushnir <jkushnir@me.com>
2020-11-30 10:03:28 +0200

    added deploy-k9s.sh

---
+++ deploy-k9s.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+wget https://github.com/derailed/k9s/releases/download/v0.24.1/k9s_Linux_x86_64.tar.gz
+tar -xvzf k9s_Linux_x86_64.tar.gz
+chmod +x k9s
+cp k9s /usr/bin/
+mv k9s /usr/local/bin/
+rm README.md
+rm LICENSE
+rm k9s_Linux_x86_64.tar.gz*
+echo Done
+
